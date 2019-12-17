194f2f5b698a165d52cd03adcda660ce6ccdd741
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2019-12-17 14:04:43 +0000

    Completed UI setup and started adding movement (joystick for mobile)

---
+++ AE3/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    private Rigidbody2D RB;
+
+    private void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 MoveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+
+        //Prevent player from moving faster on diagonals
+        if (MoveVec.magnitude > 1)
+        {
+            MoveVec = MoveVec.normalized;
+        }
+
+        RB.velocity = MoveVec * moveSpeed;
+
+        //Rotate player
+        if (CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0)
+        {
+            transform.localEulerAngles = new Vector3(0f, 0f, Mathf.Atan2(CrossPlatformInputManager.GetAxis("Vertical"), CrossPlatformInputManager.GetAxis("Horizontal")) * 180 / Mathf.PI);
+        }
+    }
+}
